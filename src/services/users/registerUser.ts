b13297cf9424d523d4afa4b86f3b4d1329ba4a80
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-11-02 20:35:10 -0300

    feat(users): check if user exists before register

--- src/services/users/registerUser.ts
+++ src/services/users/registerUser.ts
@@ -1,5 +1,6 @@
 import { User } from '.prisma/client';
 import { prisma } from '../../database/prisma';
+import { ApplicationError } from '../../errors/ApplicationError';
 import { securityService } from '../security';
 
 interface IRequest {
@@ -16,6 +17,14 @@ export const registerUser = async ({
   lastName,
 }: IRequest): Promise<User> => {
   const passwordHash = await securityService.hashString(password);
+
+  const userExists = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (userExists) {
+    throw new ApplicationError('User already registered');
+  }
 
   const user = await prisma.user.create({
     data: {
