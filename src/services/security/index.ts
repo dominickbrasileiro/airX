b3d0bcff5d26f6f2cd4379e20a4d921158776c44
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-11-05 03:40:24 -0300

    feat(security): add services to the wrapper object

--- src/services/security/index.ts
+++ src/services/security/index.ts
@@ -1,5 +1,9 @@
+import { compareHash } from './compareHash';
 import { hashString } from './hashString';
+import { generateToken } from './generateToken';
 
 export const securityService = {
   hashString,
+  compareHash,
+  generateToken,
 };
